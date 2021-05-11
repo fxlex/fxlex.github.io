92a59e396bebc74433032355e62771e6c818228f
Alexander Pann <alexander.pann@gmail.com>
2021-05-11 12:34:21 +0200

    Add new publication.

--- src/components/scientific_work.js
+++ src/components/scientific_work.js
@@ -32,6 +32,7 @@ export default class ScientificWorkComponent extends React.Component {
                 <a href="./publications/Einsatz_funktionaler_Programmierung_in_der_Webentwicklung.pdf" rel="noreferrer" target="_blank">Einsatz funktionaler Programmierung in derWebentwicklung</a><br />
             <a href="./publications/Einsatz_von_Gesten_in_der_mobilen_Softwareentwicklung.pdf" rel="noreferrer" target="_blank">Einsatz von Gesten in der mobilen Softwareentwicklung</a><br />
             <a href="./publications/Vergleich_einer_reaktiven_und_imperativen_Implementierung_eines_Demoprojektes_zum_Filtern_von_Live_Daten.pdf" rel="noreferrer" target="_blank">Reaktive und imperative Implementierung zum Filtern von Live-Daten</a><br />
+            <a href="./publications/Obfuskation_von_Quellcode_in_mobilen_und_webbasierten_Anwendungen.pdf" rel="noreferrer" target="_blank">Obfuskation von Quellcode in mobilen und webbasierten_Anwendungen</a><br />
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
